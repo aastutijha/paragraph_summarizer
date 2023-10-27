8b1cd18cff207b419e358381543a26c2e0c7f769
aastutijha <jhaastuti.ajha@gmail.com>
2023-10-27 21:43:11 +0545

    chn=anging the text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
+        Text Summarizer with <br className='max-md:hidden' />
         <span className='orange_gradient '>Custom Api</span>
       </h1>
       <h2 className='desc'>
